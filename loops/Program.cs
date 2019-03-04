d2309681c1a4a83a4cea0df48e53f755f572c422
Gjurgica Anastasovska <gurgica78@gmail.com>
2019-03-04 16:22:16 +0100

    Add project files.

---
+++ loops/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[5];
+            int result = 0;
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Enter a five numbers");
+                string answer = Console.ReadLine();
+                numbers[i] = int.Parse(answer);
+            }
+            foreach(int num in numbers)
+            {
+                result += num;
+                Console.WriteLine(num);
+                
+            }
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ loops2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loops2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string[] { "Gjurgica", "Nikola" };
+            Console.WriteLine("Enter your name");
+            Array.Resize(ref names, names.Length+1);
+            names[names.Length-1] = Console.ReadLine();
+            foreach(string name in names)
+            {
+                Console.WriteLine(name);
+            }
+            Console.ReadLine();
+            char answer;
+            do
+            {
+                Console.WriteLine("Do you want enter another name?" +
+                    "Choose 'y' for yes or 'n' for no");
+                string input = Console.ReadLine();
+                answer = char.Parse(input);
+                if (answer != 'y')
+                {
+                    return;
+                }
+                Array.Resize(ref names, names.Length + 1);
+                names[names.Length - 1] = Console.ReadLine();
+                foreach(string name in names)
+                {
+                    Console.WriteLine(name);
+                }
+                Console.ReadLine();
+
+            }
+            while (answer == 'y');
+          
+        }
+    }
+}
